c356e25b042cfa78a5da8005156dc09d6c864842
Alejandro Carayol <alejandro.carayol@gmail.com>
2019-03-06 10:06:06 +0100

    Game Structure Initialized.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+
+}
+
+function create() {
+    
+}
+
+function update() {
+
+}
